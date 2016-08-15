46a350665120848c5065a6b0927595d704233619
破天 <linfengdd@sina.com>
2016-08-15 12:22:29 +0800

    新建网页保存pdf

---
+++ WebSave_WF/WebSave.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WebSave_WF
+{
+    partial class WebSave
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WebSave));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tbxURL = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Silver;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.tbxURL);
+            this.groupBox1.Location = new System.Drawing.Point(0, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(283, 87);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "网页转换PDF格式";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSave.Location = new System.Drawing.Point(92, 58);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(96, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "保存网页为PDF";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // tbxURL
+            // 
+            this.tbxURL.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxURL.Location = new System.Drawing.Point(4, 37);
+            this.tbxURL.Name = "tbxURL";
+            this.tbxURL.Size = new System.Drawing.Size(273, 21);
+            this.tbxURL.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "请输入网页地址";
+            // 
+            // WebSave
+            // 
+            this.AcceptButton = this.btnSave;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(284, 82);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("楷体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "WebSave";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "HTML2PDF";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbxURL;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WebSave_WF/WebSave.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using ExpertPdf;
+using ExpertPdf.HtmlToPdf;
+
+namespace WebSave_WF
+{
+    public partial class WebSave : Form
+    {
+        #region 属性
+        #endregion
+        #region 构造器
+        public WebSave()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region 事件
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ValidateURL();
+                saveFileDialog1.Filter = "pdf files(*.pdf)|*.pdf";//设置文件选择器
+                saveFileDialog1.DefaultExt = "pdf";//设置后缀
+                //获取或设置一个值，该值指示如果用户省略扩展名，文件对话框是否自动在文件名中添加扩展名。（可以不设置）
+                saveFileDialog1.AddExtension = true;
+                //保存对话框是否记忆上次打开的目录
+                saveFileDialog1.RestoreDirectory = true;
+                //默认桌面
+                saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string htmlTitle = GetHTMLTitle(tbxURL.Text.Trim()).Trim();
+                htmlTitle = this.TrimFileName(htmlTitle);//去掉文件名不支持的字符
+                saveFileDialog1.FileName = htmlTitle.Length > 255 ? htmlTitle.Substring(0, 255) : htmlTitle;
+                DialogResult result= saveFileDialog1.ShowDialog();
+                string localFilePath = "", fileNameExt = "", newFileName = "", FilePath = "";
+                if (result == DialogResult.OK)
+                {
+                    //获得文件路径
+                    localFilePath = saveFileDialog1.FileName.ToString();
+
+                    //获取文件名，不带路径
+                    //fileNameExt = localFilePath.Substring(localFilePath.LastIndexOf("\\") + 1);
+
+                    //获取文件路径，不带文件名
+                    //FilePath = localFilePath.Substring(0, localFilePath.LastIndexOf("\\"));
+
+                    //给文件名前加上时间
+                    //newFileName = DateTime.Now.ToString("yyyyMMdd") + fileNameExt;
+
+                    //在文件名里加字符
+                    //saveFileDialog.FileName.Insert(1,"dameng");
+                    //为用户使用 SaveFileDialog 选定的文件名创建读/写文件流。
+                    //System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog.OpenFile();//输出文件
+
+                    //fs可以用于其他要写入的操作
+                    Use_ephtmltopdf(tbxURL.Text.Trim(), localFilePath);
+                    string OKMessage = string.Format("网页保存成功！");
+                    MessageBox.Show(OKMessage);
+                    this.WindowState = FormWindowState.Minimized;
+                }
+
+            }
+            catch (WarningException warning)
+            {
+                MessageBox.Show(warning.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+       
+       
+
+        #endregion
+        #region 方法
+        /// <summary>
+        /// 验证URL
+        /// </summary>
+        private void ValidateURL()
+        {
+            if (0 == tbxURL.Text.Trim().Length)
+                throw new WarningException("请输入URL！");
+        }
+        /// <summary>
+        /// 获取HTML标题
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        private string GetHTMLTitle(string url)
+        {
+            string URI = url;
+            HttpWebRequest request = WebRequest.Create(URI) as HttpWebRequest;
+            request.Method = "GET";
+            request.KeepAlive = true;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            System.IO.Stream responseStream = response.GetResponseStream();
+            System.IO.StreamReader reader = new System.IO.StreamReader(responseStream, Encoding.UTF8);
+            //返回的页面html文本
+            string srcString = reader.ReadToEnd();
+            Regex regex = new Regex("<Title>[\\s\\S]*</Title>",RegexOptions.IgnoreCase);
+            MatchCollection matchCollection = regex.Matches(srcString, 0);
+            if (matchCollection.Count == 0) return string.Empty;
+            else {
+                string strTitle = matchCollection[0].Value;
+                return ParseTags(strTitle);
+            
+            }
+        }
+        /// <summary>
+        /// 生成PDF
+        /// </summary>
+        /// <param name="URL">URL</param>
+        /// <param name="outFile">输入文件名</param>
+        public void Use_ephtmltopdf(string URL, string outFile)
+        {
+            PdfConverter pdfConverter = new PdfConverter();
+            pdfConverter.PdfDocumentOptions.EmbedFonts = false;
+            pdfConverter.PdfDocumentOptions.ShowFooter = false;
+            pdfConverter.PdfDocumentOptions.ShowHeader = false;
+            pdfConverter.PdfDocumentOptions.GenerateSelectablePdf = true;
+            pdfConverter.SavePdfFromUrlToFile(URL, outFile);
+        }
+        /// <summary>
+        /// 去掉文件命中不支持的字符
+        /// \、/、:、*、?、"、<、>、| 。
+        /// </summary>
+        /// <param name="htmlTitle"></param>
+        /// <returns></returns>
+        /// 
+        private string TrimFileName(string htmlTitle)
+        {
+            return Regex.Replace(htmlTitle, @"[\\\/:\*?<>\|""]", "", RegexOptions.IgnoreCase);
+        }
+        ///   <summary>
+        ///   移除所有HTML标签
+        ///   </summary>
+        ///   <param   name="htmlStr">HTMLStr</param>
+        public static string ParseTags(string htmlStr)
+        {
+            return Regex.Replace(htmlStr, "<[^>]*>", "");
+        }
+        //*************************************************
+        ///   <summary>
+        ///   去除HTML标记
+        ///   </summary>
+        ///   <param   name="NoHTML">包括HTML的源码   </param>
+        ///   <returns>已经去除后的文字</returns>
+        public static string NoHTML(string htmlString)
+        {
+            //删除脚本
+            htmlString = Regex.Replace(htmlString, @"<script[^>]*?>.*?</script>", "",
+              RegexOptions.IgnoreCase);
+            //删除HTML
+            htmlString = Regex.Replace(htmlString, @"<(.[^>]*)>", "",
+              RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"([\r\n])[\s]+", "",
+              RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"-->", "", RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"<!--.*", "", RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"&(quot|#34);", "\"",
+              RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"&(amp|#38);", "&",
+              RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"&(lt|#60);", "<",
+              RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"&(gt|#62);", ">",
+              RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"&(nbsp|#160);", "   ",
+              RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"&(iexcl|#161);", "\xa1",
+              RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"&(cent|#162);", "\xa2",
+              RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"&(pound|#163);", "\xa3",
+              RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"&(copy|#169);", "\xa9",
+              RegexOptions.IgnoreCase);
+            htmlString = Regex.Replace(htmlString, @"&#(\d+);", "",
+              RegexOptions.IgnoreCase);
+            htmlString.Replace("<", "");
+            htmlString.Replace(">", "");
+            htmlString.Replace("\r\n", "");
+            return htmlString;
+        }
+        #endregion
+    }
+}
